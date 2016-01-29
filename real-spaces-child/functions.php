a6f0fb3b1808dd9dc8ebc898809816db80e150fd
AbleLincoln <amaney@usc.edu>
2016-01-28 18:59:33 -0800

    Google analytics in function

--- real-spaces-child/functions.php
+++ real-spaces-child/functions.php
@@ -498,4 +498,17 @@ function imic_agent_register() {
 add_action('wp_ajax_nopriv_imic_agent_register', 'imic_agent_register');
 add_action('wp_ajax_imic_agent_register', 'imic_agent_register');
 
+add_action('wp_footer', 'add_googleanalytics');
+function add_googleanalytics() { ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-73067924-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+<?php }
+
 ?>
